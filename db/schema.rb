758b30684ec84c14ec474a9811c0fdd57df2d3bf
bheeshma <bheeshmat@gmail.com>
2016-02-18 17:22:59 +0530

    Generate Task Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217042135) do
+ActiveRecord::Schema.define(version: 20160218114404) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -29,5 +29,13 @@ ActiveRecord::Schema.define(version: 20160217042135) do
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "status",      default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
 end
