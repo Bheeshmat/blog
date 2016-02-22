7d31001ded83bb54560b793d1d07268ee2fab762
bheeshma <bheeshmat@gmail.com>
2016-02-22 20:08:10 +0530

    Authenticate user before managing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,9 @@
 
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret",
-  except: [:index, :show]
+  # http_basic_authenticate_with name: "dhh", password: "secret",
+  # except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @article = Article.all
